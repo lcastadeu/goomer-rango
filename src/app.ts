bfc630a40e9e083eee122b34ada52692c7eef5e3
Lucas Tadeu H. Lustosa <lcas_tadeu@hotmail.com>
2021-08-25 22:58:45 -0300

    Disponibilizado script Startup SQL

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
 import express from 'express';
 import restauranteRoute from './application/route/resturante.route';
+import { HttpRequestCode } from './infraestructure/enum/http_request_code.enum';
 import { ReturnMessage } from './infraestructure/return_message';
+import database from './infraestructure/database/database.connection';
+import { readFileSync } from 'fs';
+import * as path from 'path';
+
 
 const api = express();
 
 api.get('/', function (_, res) {
-  res.status(200).json(new ReturnMessage('Serviço Ativo!'));
+  res.status(HttpRequestCode.Ok).json(new ReturnMessage('Serviço Ativo!'));
 });
 
 api.use('/api', restauranteRoute);
 
 api.listen(3000, async () => {
+
+  const scriptStartup = readFileSync(path.join(__dirname, '/infraestructure/seed/startup.sql'), { encoding: 'utf-8' })
+  await database.connect().then(async pool => {
+    await pool.query(scriptStartup);
+  });
+
   console.log('Serviço Ativo');
 });
 
