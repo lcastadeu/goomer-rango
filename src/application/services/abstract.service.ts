91b920fdea2aabfc3bbb10762409a5d6cd89e3a6
Lucas Tadeu H. Lustosa <lcas_tadeu@hotmail.com>
2021-08-26 06:51:42 -0300

    Realizado ajustes no service de restaurante

--- src/application/services/abstract.service.ts
+++ src/application/services/abstract.service.ts
@@ -1,10 +1,8 @@
-import { DatabaseCommand } from '../../infraestructure/database/database.command';
+
 export class AbstractService {
 
-  protected command: DatabaseCommand;
-
   constructor() {
-    this.command = new DatabaseCommand();
+   
   }
 
 
--- src/application/services/restaurante.servive.ts
+++ src/application/services/restaurante.servive.ts
@@ -1,8 +1,20 @@
 import { AbstractService } from './abstract.service';
+import { Restaurante } from '../../domain/entities/restaurante.entity';
 export class RestauranteService extends AbstractService {
+  
+  async find(id: number) {
 
-  async get(id?: number) {
-    
   }
 
+  async findAll() {
+
+  }
+
+  async save(restaurante: Restaurante) {
+
+  }
+
+  async delete(id: number) {
+
+  }
 }
